31ba428bfb4d15f42087767f4da883185b3061c2
Ice123239 <s6752410018@sau.ac.th>
2026-08-26 11:26:49 +0700

    first commit

---
+++ app/register/page.tsx
@@ -0,0 +1,24 @@
+// หน้า HOME PAGE Root -> /
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>ID: 6752410018</h1>
+      <h1>Name: Pariwat</h1>
+      <hr />
+      
+      <h3>Link to Basic Routing Page</h3>
+      <Link href="/register">[Register Page]</Link><br />
+      <Link href="/products">[Products Page]</Link>
+
+      <hr />
+      
+      <h3>Link to Nested Routing Page</h3>
+      <Link href="/products/keyboard">Keyboard Page</Link><br />
+      <Link href="/products/mouse">Mouse Page</Link><br />
+      <Link href="/notebook/acer">Notebook Acer Page</Link><br />
+      <Link href="/notebook/lenovo">Notebook Lenovo Page</Link><br />
+    </div>
+  );
+}
